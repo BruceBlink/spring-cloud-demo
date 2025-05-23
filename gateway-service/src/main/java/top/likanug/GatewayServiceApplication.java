35a09b173427f978311b9d17531531fc16784a66
likanug <likanug.g@qq.com>
2025-05-23 14:18:35 +0800

    重命名个服务名称及模块名称

--- gateway-service/src/main/java/top/likanug/GatewayServiceApplication.java
+++ gateway-service/src/main/java/top/likanug/GatewayServiceApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayServerApplication {
+public class GatewayServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayServerApplication.class, args);
+        SpringApplication.run(GatewayServiceApplication.class, args);
     }
 }
